4a464b36a0b732bc24d310c4714e0b2bfa9434b6
juanmayorquin <juanjmayor@outlook.es>
2023-11-14 21:04:59 -0500

    Listo para implementar audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,55 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { data } from "./data/temp_data";
-import { useState } from "react";
 
 function App() {
-  let temp = "+0";
-  const [bgColor, setBgColor] = useState("bg-black");
-
-  const updateBgColor = () => {
-    if (temp > 0.35){
-      setBgColor("bg-barn_red")
-    }
-  };
+  const [year, setYear] = useState(1880);
+  const [temp, setTemp] = useState(0);
+  const [bgColor, setBgColor] = useState("black");
 
   useEffect(() => {
-    updateBgColor();
-  }, []);
+    const yearData = data.find((d) => d.year === year);
+    if (yearData) {
+      setTemp(yearData.temperature);
+    }
+  }, [year]);
+
+  useEffect(() => {
+    let newBgColor;
+    if (temp >= 0.7) {
+      newBgColor = "barn_red";
+    } else if (0.36 <= temp && temp < 0.7) {
+      newBgColor = "fire_brick";
+    } else if (0.05 <= temp && temp < 0.36) {
+      newBgColor = "carrot_orange";
+    } else if (-0.17 <= temp && temp < 0.05) {
+      newBgColor = "teal";
+    } else if (temp < -0.17) {
+      newBgColor = "forest_green";
+    }
+    setBgColor(newBgColor);
+  }, [temp]);
+
+  const handleYearChange = (e) => {
+    setYear(Number(e.target.value));
+  };
 
   return (
-    <section className={`h-screen ${bgColor} text-white transition-all`}>
-      <div className="flex w-full justify-center items-center">
+    <section
+      className="h-screen text-white transition-all duration-200"
+      style={{ backgroundColor: `var(--${bgColor})` }}
+    >
+      <div className="flex flex-col h-full w-full justify-between text-center py-10">
         <h2>{temp}°C</h2>
+        <h2 className="text-8xl font-semibold">{year}</h2>
+        <input
+          id="myinput"
+          type="range"
+          min="1880"
+          max="2022"
+          value={year}
+          onChange={handleYearChange}
+          className="max-w-5xl w-10/12 mx-auto h-10"
+        />
       </div>
     </section>
   );
